27531d869f7fed5cc7a81c62111baae5be28c02b
Leonardo Ribeiro <lviniciusribeiro@yahoo.com.br>
2018-05-22 22:15:24 -0300

    Criado Model Post + Migration Posts

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,24 @@ from django.db import models
 
 class Category(models.Model):
 	name = models.CharField(max_length=255)
+
+class Post(models.Model):
+
+	# AQUI EM CIMA EU CONSIGO CRIAR UMA CHAVE ESTRANGEIRA FACILMENTE
+	# EU CRIO UMA COLUNA CHAMADA CATEGORY QUE E UMA FOREIGNKEY LIGADA NA MODEL CATEGORY.
+
+	category = models.ForeignKey(Category)
+	name = models.CharField(max_length=255)
+	content = models.TextField()
+	# Esse STATUS_CHOICE E UMA TUPLA DE OPCAO, ONDE ESTOU DIZENDO QUE ESSA COLUNA NO BANCO 
+	# NÃO PODE SER MODIFICADA, APENAS UM SELECT COM ESSAS OPCOES 
+	STATUS_CHOICE = (
+		('Draft','Draft'),
+		('Publish','Publish')
+	)
+
+	# AQUI EMBAIXO EU CRIO UMA COLUNA CHAMADA STATUS NO BANCO, QUE RECEBE A TUPLA ACIMA
+	# COM VALOR DEFAULT DE DRAFT
+
+	status = models.CharField(max_length=20, choices=STATUS_CHOICE, default='Draft')
+	created_at = models.DateTimeField(auto_now_add=True)
